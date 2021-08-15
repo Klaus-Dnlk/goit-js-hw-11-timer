0592b9bb8e4ec401a0fa8e405c9bc263684fafd9
Klaus-Dnlk <79231530+Klaus-Dnlk@users.noreply.github.com>
2021-08-15 12:28:00 +0300

    исправил остановку таймера

--- index.js
+++ index.js
@@ -2,36 +2,48 @@ class CountdownTimer {
   constructor() {
     this.in ={
       selector: '#timer-1',
-      targetDate: new Date('Aug 30, 2021')
+      targetDate: new Date('Aug 28, 2021'),
+      currentDate: null,
+      id: null
     };
 
     this.refs = {
+      body: document.querySelector('body'),
       days: document.querySelector('[data-value="days"]'),
       hours: document.querySelector('[data-value="hours"]'),
       mins: document.querySelector('[data-value="mins"]'),
       secs: document.querySelector('[data-value="secs"]')
-    }        
+    }
   }
-    
+
   calc = () => {
+    this.in.currentDate = Date.now();
+    const time = this.in.targetDate - this.in.currentDate;
+
+    if (this.in.targetDate <= this.in.currentDate) {
+      clearInterval(this.in.id);
+      return;
+    }    
         
-    const currentDate = Date.now();
-    const time = this.in.targetDate - currentDate;    
     const days = Math.floor(time / (1000 * 60 * 60 * 24));    
     const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
     const secs = Math.floor((time % (1000 * 60)) / 1000);    
-    this.refs.days.textContent = days;
-    this.refs.hours.textContent = hours < 10 ? `0${hours}` : hours;
-    this.refs.mins.textContent = mins < 10 ? `0${mins}` : mins;
-    this.refs.secs.textContent = secs < 10 ? `0${secs}` : secs;
-  }
 
-  timerStart = () => {
-    this.currentDate = Date.now();
-    setInterval(this.calc, 1000);
+    this.refs.days.textContent = days;
+    this.refs.hours.textContent = String(hours).padStart(2, '0');
+    this.refs.mins.textContent = String(mins).padStart(2, '0');
+    this.refs.secs.textContent = String(secs).padStart(2, '0');
+
+    
   }
+  
+  timerStart = () => {
+      
+      this.in.id = setInterval(this.calc, 1000);
+ }  
 } 
- 
 const deadlineCount = new CountdownTimer();
 window.addEventListener("DOMContentLoaded", deadlineCount.timerStart);
+
+
